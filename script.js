1d409d38a9ac430b737856e9a64f6540a600b3c1
Chris Vinson Kunnankada <chrisvinsonk@gmail.com>
2023-12-04 13:20:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+const form = document.getElementById('bank-form');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const name = document.getElementById('name').value;
+    const accountNumber = document.getElementById('account-number').value;
+    const routingNumber = document.getElementById('routing-number').value;
+
+    let errors = [];
+
+    if (name.trim() === '') {
+        errors.push('Please enter your full name');
+    }
+
+    if (accountNumber.length !== 16) {
+        errors.push('Account number must be 16 digits long');
+    }
+
+    if (routingNumber.length !== 9) {
+        errors.push('Routing number must be 9 digits long');
+    }
+
+    if (errors.length > 0) {
+        alert(errors.join('\n'));
+        return;
+    }
+
+    // TODO: Implement your logic to handle valid data (e.g., API call, storage)
+
+    alert('Thank you! Your banking details have been submitted.');
+    alert('You have been successfully phished!!!! Please refrain from entering your banking details into unverified sites in the future. We have your banking details now thank you for the free money!!!');
+});
